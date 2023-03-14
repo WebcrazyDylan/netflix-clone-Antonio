0339182b9bb3f4c930fd67870e64de07e8c7f85b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-14 11:20:32 +0530

    added input component for signing in

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,3 +1,4 @@
+import Input from "@/components/Input";
 import React from "react";
 
 const Auth = () => {
@@ -9,7 +10,10 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            Log In
+            <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
+            <div className="flex flex-col gap-4">
+              <Input />
+            </div>
           </div>
         </div>
       </div>
