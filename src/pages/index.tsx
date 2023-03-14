61106b644823dbfce9ee7f21a6618be645b6d111
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-14 10:11:14 +0530

    added netflix favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <title>Netflix Clone</title>
         <meta name="description" content="Netflix Clone" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/netflix.svg" />
       </Head>
       <h1 className="text-3xl font-bold underline">Netflix Clone</h1>
     </>
