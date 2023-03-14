4090b59e94f58b373fd98eb4f4ba45beb4746479
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-14 18:51:47 +0530

    added and init prisma

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Input from "@/components/Input";
 import React, { useCallback, useState } from "react";
 
@@ -15,71 +16,80 @@ const Auth = () => {
   }, []);
 
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
-      <div className="bg-black w-full h-full lg:bg-opacity-50">
-        <nav className="px-12 py-5">
-          <img src="/images/logo.png" alt="Logo" className="h-12" />
-        </nav>
-        <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === "login" ? "Sign In" : "Register"}
-            </h2>
-            <div className="flex flex-col gap-4">
-              {variant === "register" && (
+    <>
+      <Head>
+        <title>Log In / Sign Up</title>
+        <meta name="description" content="Netflix Clone" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/netflix.svg" />
+      </Head>
+
+      <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
+        <div className="bg-black w-full h-full lg:bg-opacity-50">
+          <nav className="px-12 py-5">
+            <img src="/images/logo.png" alt="Logo" className="h-12" />
+          </nav>
+          <div className="flex justify-center">
+            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+              <h2 className="text-white text-4xl mb-8 font-semibold">
+                {variant === "login" ? "Sign In" : "Register"}
+              </h2>
+              <div className="flex flex-col gap-4">
+                {variant === "register" && (
+                  <Input
+                    label="Username"
+                    onChange={(
+                      event: React.ChangeEvent<
+                        HTMLTextAreaElement | HTMLInputElement
+                      >
+                    ) => setName(event.target.value)}
+                    id="name"
+                    type="name"
+                    value={name}
+                  />
+                )}
                 <Input
-                  label="Username"
+                  label="E-Mail"
                   onChange={(
                     event: React.ChangeEvent<
                       HTMLTextAreaElement | HTMLInputElement
                     >
-                  ) => setName(event.target.value)}
-                  id="name"
-                  type="name"
-                  value={name}
+                  ) => setEmail(event.target.value)}
+                  id="email"
+                  type="email"
+                  value={email}
                 />
-              )}
-              <Input
-                label="E-Mail"
-                onChange={(
-                  event: React.ChangeEvent<
-                    HTMLTextAreaElement | HTMLInputElement
-                  >
-                ) => setEmail(event.target.value)}
-                id="email"
-                type="email"
-                value={email}
-              />
-              <Input
-                label="Password"
-                onChange={(
-                  event: React.ChangeEvent<
-                    HTMLTextAreaElement | HTMLInputElement
-                  >
-                ) => setPassword(event.target.value)}
-                id="password"
-                type="password"
-                value={password}
-              />
+                <Input
+                  label="Password"
+                  onChange={(
+                    event: React.ChangeEvent<
+                      HTMLTextAreaElement | HTMLInputElement
+                    >
+                  ) => setPassword(event.target.value)}
+                  id="password"
+                  type="password"
+                  value={password}
+                />
+              </div>
+              <button className="bg-red-500 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                {variant === "login" ? "Log In" : "Sign Up"}
+              </button>
+              <p className="text-neutral-400 mt-12">
+                {variant === "login"
+                  ? "First time using Netflix?"
+                  : "Already have an account?"}
+                <span
+                  onClick={toggleVariant}
+                  className="text-white ml-1 hover:underline cursor-pointer"
+                >
+                  {variant === "login" ? "Create an Account." : "Log In"}
+                </span>
+              </p>
             </div>
-            <button className="bg-red-500 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              {variant === "login" ? "Log In" : "Sign Up"}
-            </button>
-            <p className="text-neutral-400 mt-12">
-              {variant === "login"
-                ? "First time using Netflix?"
-                : "Already have an account?"}
-              <span
-                onClick={toggleVariant}
-                className="text-white ml-1 hover:underline cursor-pointer"
-              >
-                {variant === "login" ? "Create an Account." : "Log In"}
-              </span>
-            </p>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
