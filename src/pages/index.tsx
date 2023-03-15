b2e24e1be8af30e4462a4f31036be6c511074061
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-15 10:17:26 +0530

    added new comp Navbar and NavbarItem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import { NextPageContext } from "next";
-import useCurrentUser from "@/hooks/useCurrentUser";
+
+import Navbar from "@/components/Navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,8 +22,6 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
   return (
     <>
       <Head>
@@ -31,14 +30,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/netflix.svg" />
       </Head>
-      <h1 className="text-4xl font-bold underline text-white">Netflix Clone</h1>
-      <p className="text-white">Logged In As: {user?.name}</p>
-      <button
-        className="h-10 w-full text-white cursor-pointer"
-        onClick={() => signOut()}
-      >
-        Log Out!
-      </button>
+
+      <Navbar />
     </>
   );
 }
