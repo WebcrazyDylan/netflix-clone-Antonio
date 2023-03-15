e4bd765f5333a1df3fceffef94150abebb59cfce
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-15 08:53:06 +0530

    added .prettierrc for code formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,19 @@
 import Head from "next/head";
+import { getSession, signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false
+      }
+    };
+  }
+}
 
 export default function Home() {
   return (
@@ -9,7 +24,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/netflix.svg" />
       </Head>
-      <h1 className="text-3xl font-bold underline">Netflix Clone</h1>
+      <h1 className="text-4xl font-bold underline text-white">Netflix Clone</h1>
+      <button
+        className="h-10 w-full text-white cursor-pointer"
+        onClick={() => signOut()}
+      >
+        Log Out!
+      </button>
     </>
   );
 }
